3a57d6687071fb22eac57a3fcb47401bfac619e4
Cristian <cribauma@gmail.com>
2023-12-06 22:03:10 +0100

    Formulario situacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Formulario from './components/Form.jsx';
 
 function App() {
+  
+  const manejarDatosFormulario = (datos) => {
+    // Haz algo con los datos del formulario aquí
+    console.log(datos);
+  };
+
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+    <div>
+    <h1>WarMedAI</h1>
+    <div className="flex justify-center">
+      <img src="/soldier.png"  className="w-1/4" />
+    </div>
+    <Formulario onEnviar={manejarDatosFormulario}/>
+  </div>
   )
 }
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+
+const Formulario = ({ onEnviar }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    onEnviar(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto mt-4 p-4 bg-white rounded shadow">
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Identificador del Accidentado:
+        </label>
+        <input {...register("identificador")} className="mt-1 p-2 border rounded w-full" />
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Respira?
+        </label>
+          <label className="mr-4">
+            Sí
+            <input type="radio" {...register("respira")} value="Sí" className="ml-2" />
+          </label>
+          <label>
+            No
+            <input type="radio" {...register("respira")} value="No" className="ml-2" />
+          </label>
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Está consciente?
+        </label>
+          <label className="mr-4">
+            Sí
+            <input type="radio" {...register("consciente")} value="Sí" className="ml-2" />
+          </label>
+          <label>
+            No
+            <input type="radio" {...register("consciente")} value="No" className="ml-2" />
+          </label>
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-600">
+          Situación:
+        </label>
+        <textarea {...register("situacion")} className="mt-1 p-2 border rounded w-full" />
+      </div>
+
+      <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Enviar
+      </button>
+    </form>
+  );
+};
+
+Formulario.propTypes = {
+  onEnviar: PropTypes.func.isRequired,
+};
+
+export default Formulario;
